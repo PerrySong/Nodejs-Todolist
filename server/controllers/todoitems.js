08f91b3a876e6042ce640dea865b39b4101bb156
spfscmz <psong4@dons.usfca.edu>
2018-06-07 20:03:59 -0700

    Still has bugs

--- server/controllers/todoitems.js
+++ server/controllers/todoitems.js
@@ -10,4 +10,49 @@ module.exports = {
       .then(todoItem => res.status(201).send(todoItem))
       .catch(error => res.status(400).send(error));
   },
+  update(req, res) {
+    return TodoItem
+      .find({
+          where: {
+            id: req.params.todoItemId,
+            todoId: req.params.todoId,
+          },
+        })
+      .then(todoItem => {
+        if (!todoItem) {
+          return res.status(404).send({
+            message: 'TodoItem Not Found',
+          });
+        }
+
+        return todoItem
+          .update(req.body, { fields: Object.keys(req.body) })
+          .then(updatedTodoItem => res.status(200).send(updatedTodoItem))
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  },
+
+  destroy(req, res) {
+    return TodoItem
+      .find({
+          where: {
+            id: req.params.todoItemId,
+            todoId: req.params.todoId,
+          },
+        })
+      .then(todoItem => {
+        if (!todoItem) {
+          return res.status(404).send({
+            message: 'TodoItem Not Found',
+          });
+        }
+
+        return todoItem
+          .destroy()
+          .then(() => res.status(204).send())
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  },
 };
